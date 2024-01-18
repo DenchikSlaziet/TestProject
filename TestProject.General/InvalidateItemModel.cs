84db94d0563bb19a917428f09306716fda32c649
Денис Кочетков <106813642+DenchikSlaziet@users.noreply.github.com>
2024-01-19 00:40:52 +0300

    Написал сервисы и валидаторы (#6)

--- TestProject.General/InvalidateItemModel.cs
+++ TestProject.General/InvalidateItemModel.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TestProject.General
+﻿namespace TestProject.General
 {
-    internal class InvalidateItemModel
+    /// <summary>
+    /// Модель инвалидации запросов
+    /// </summary>
+    public class InvalidateItemModel
     {
+        /// <summary>
+        /// Создаёт <see cref="InvalidateItemModel"/>
+        /// </summary>
+        public static InvalidateItemModel New(string field, string message)
+            => new InvalidateItemModel(field, message);
+
+        /// <summary>
+        /// Инициализирует новый экземпляр <see cref="InvalidateItemModel"/>
+        /// </summary>
+        private InvalidateItemModel(string field, string message)
+        {
+            Field = field;
+            Message = message;
+        }
+
+        /// <summary>
+        /// Имя инвалидного поля
+        /// </summary>
+        /// <remarks>Если пустое, значит инвалидация относится ко всей моделе</remarks>
+        public string Field { get; }
+
+        /// <summary>
+        /// Сообщение инвалидации
+        /// </summary>
+        public string Message { get; }
     }
 }
